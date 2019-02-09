219e2c417a7c16470304a13f65e60d346ded9843
camilo vargas <cvargasleon@artesyletras.edu.co>
2019-02-09 12:17:31 -0500

    completado

---
+++ js.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+<script>
+function myFunction() {
+  var x = document.getElementById("menu-movil");
+  if (x.className === "menug") {
+    x.className += " responsive";
+  } else {
+    x.className = "menug";
+  }
+}
+</script>
